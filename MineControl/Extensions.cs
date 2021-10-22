c298feba34ebe976e04303d8c05e3079699e5b79
smurferson1 <smurferson1@protonmail.com>
2021-10-22 12:20:11 -0700

    Rewrote TreeView moveup/down code that previously relied on StackOverflow.

--- MineControl/Extensions.cs
+++ MineControl/Extensions.cs
@@ -4,54 +4,28 @@
 namespace System.Windows.Forms
 {
     public static class Extensions
-    {
-        // from https://stackoverflow.com/questions/2203975/move-node-in-tree-up-or-down
+    {        
         public static void MoveUp(this TreeNode node)
         {
-            TreeNode parent = node.Parent;
-            TreeView view = node.TreeView;
-            if (parent != null)
+            TreeNodeCollection parentNodes = node.Parent == null ? node.TreeView.Nodes : node.Parent.Nodes;
+            int nodeIndex = node.Index;
+
+            if (nodeIndex > 0)
             {
-                int index = parent.Nodes.IndexOf(node);
-                if (index > 0)
-                {
-                    parent.Nodes.RemoveAt(index);
-                    parent.Nodes.Insert(index - 1, node);
-                }
-            }
-            else if (node.TreeView.Nodes.Contains(node)) //root node
-            {
-                int index = view.Nodes.IndexOf(node);
-                if (index > 0)
-                {
-                    view.Nodes.RemoveAt(index);
-                    view.Nodes.Insert(index - 1, node);
-                }
+                parentNodes.Remove(node);
+                parentNodes.Insert(nodeIndex - 1, node);
             }
         }
 
-        // from https://stackoverflow.com/questions/2203975/move-node-in-tree-up-or-down
         public static void MoveDown(this TreeNode node)
         {
-            TreeNode parent = node.Parent;
-            TreeView view = node.TreeView;
-            if (parent != null)
+            TreeNodeCollection parentNodes = node.Parent == null ? node.TreeView.Nodes : node.Parent.Nodes;
+            int nodeIndex = node.Index;
+
+            if (nodeIndex < parentNodes.Count - 1)
             {
-                int index = parent.Nodes.IndexOf(node);
-                if (index < parent.Nodes.Count - 1)
-                {
-                    parent.Nodes.RemoveAt(index);
-                    parent.Nodes.Insert(index + 1, node);
-                }
-            }
-            else if (view != null && view.Nodes.Contains(node)) //root node
-            {
-                int index = view.Nodes.IndexOf(node);
-                if (index < view.Nodes.Count - 1)
-                {
-                    view.Nodes.RemoveAt(index);
-                    view.Nodes.Insert(index + 1, node);
-                }
+                parentNodes.Remove(node);
+                parentNodes.Insert(nodeIndex + 1, node);
             }
         }
         
